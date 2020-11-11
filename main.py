3c4cfe147151f3bc05281264fbb4747d0262ddc5
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2020-11-12 00:28:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import time
+from datetime import datetime
+from pynput.keyboard import Controller, Key
+from data import lst
+import webbrowser
+
+keyboard = Controller()
+
+isStarted = False
+
+for i in lst:
+    while True:
+        if isStarted == False:
+            if datetime.now().hour == int(i[1].split(':')[0]) and datetime.now().minute == int(i[1].split(':')[1]):
+                webbrowser.open(i[0])
+                isStarted = True
+            elif isStarted == True:
+                if datetime.now().hour == int(i[2].split(':')[0]) and datetime.now().minute == int(i[2].split(':')[1]):
+                    keyboard.press('w')
+                    time.sleep(1)
+                    keyboard.press(Key.enter)
+                    isStarted = False
+                    break
