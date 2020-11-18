0bc8b0680169a76ca567563ff57563d6478a463a
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2020-11-18 21:29:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ for i in lst:
             if datetime.now().hour == int(i[1].split(':')[0]) and datetime.now().minute == int(i[1].split(':')[1]):
                 webbrowser.open(i[0])
                 isStarted = True
-            elif isStarted == True:
-                if datetime.now().hour == int(i[2].split(':')[0]) and datetime.now().minute == int(i[2].split(':')[1]):
-                    keyboard.press('w')
-                    time.sleep(1)
-                    keyboard.press(Key.enter)
-                    isStarted = False
-                    break
+        elif isStarted == True:
+            if datetime.now().hour == int(i[2].split(':')[0]) and datetime.now().minute == int(i[2].split(':')[1]):
+                keyboard.press('w')
+                time.sleep(1)
+                keyboard.press(Key.enter)
+                isStarted = False
+                break
