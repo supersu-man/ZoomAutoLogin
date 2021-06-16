5e532eee867305d6f0cab3ff8b14fa8c018ebfd0
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2021-06-16 16:22:04 +0530

    Making sure if this still works
    Create image1.png
    Create image2.png
    Create README.md
    Update data.py

---
+++ README.md
@@ -0,0 +1,10 @@
+# ZoomAutoLogin
+This is a very basic python code which automates the process of joining and leaving the zoom meetings. 
+
+# Requirements 
+ - Put a random zoom link in your browser address bar and hit enter. Make sure to check [this](docs/image1.png) box and click open so you don't have to do it again and again after automating.
+ - Make sure to disable [this](docs/image2.png) 'ask for confirmation while you exit a meeting' box in your zoom settings.
+ - Always give time in 24hr format inside [data.py](data.py).
+ 
+# Support
+ - Make sure to watch [this](https://www.youtube.com/watch?v=me23_XFV3OY) video on YouTube before reporting an issue.
--- main.py
+++ main.py
@@ -10,14 +10,15 @@ isStarted = False
 
 for i in lst:
     while True:
-        if isStarted == False:
+        if not isStarted:
             if datetime.now().hour == int(i[1].split(':')[0]) and datetime.now().minute == int(i[1].split(':')[1]):
                 webbrowser.open(i[0])
                 isStarted = True
-        elif isStarted == True:
+        else:
             if datetime.now().hour == int(i[2].split(':')[0]) and datetime.now().minute == int(i[2].split(':')[1]):
                 keyboard.press('w')
                 time.sleep(1)
                 keyboard.press(Key.enter)
                 isStarted = False
                 break
+        time.sleep(5)
